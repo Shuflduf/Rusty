511ff3185525b5cdd631fdedf210613d0e822218
Shuflduf <shuflduf@gmail.com>
2024-10-18 00:15:30 -0600

    channel specific chats

--- ai.py
+++ ai.py
@@ -3,7 +3,14 @@ import google.generativeai as genai
 genai.configure(api_key="AIzaSyCW8kjw3V-YdNu1AX_qewTgFOHiOoQOywk")
 model = genai.GenerativeModel("gemini-1.5-flash")
 
+chats = {}
+
 def generate(prompt):
     response = model.generate_content(prompt)
     return response.text[:2000]
 
+def continue_chat(prompt, channel_id):
+    if not channel_id in chats.keys():
+        chats.update({channel_id: model.start_chat()})
+
+    return chats[channel_id].send_message(prompt).text[:2000]
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 conversation_mode = False
+last_channel_id = 0
 
 log.init()
 
@@ -17,8 +18,15 @@ async def on_ready():
 @client.event
 async def on_message(message):
     global conversation_mode
+    global last_channel_id
     if message.author == client.user:
         return
+    
+    if message.channel.id != last_channel_id:
+        last_channel_id = message.channel.id 
+        log_text(
+            f"[Now in {message.guild.name}: #{message.channel.name}]".center(40, '=')
+        )
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
@@ -31,7 +39,7 @@ async def on_message(message):
  
     elif conversation_mode:
         log_text(f"{message.author.name}: {message.content}")
-        response = ai.generate(message.content)
+        response = ai.continue_chat(message.content, message.channel.id)
         await message.channel.send(response)
         log_text(f"{client.user.name}: {response}")
     
