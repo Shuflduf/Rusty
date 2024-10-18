74a3e874ba620eabbf1c4f77ec473f9338ae17f4
Shuflduf <shuflduf@gmail.com>
2024-10-17 22:01:21 -0600

    GOOD LOGS

--- main.py
+++ main.py
@@ -1,41 +1,45 @@
 import discord
 import ai
+import log
+from log import log_text
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 client = discord.Client(intents=intents)
 
 conversation_mode = False
 
-async def on_ready():
-    print(f'We have logged in as {client.user}')
+log.init()
 
+async def on_ready():
+    log_text(f'We have logged in as {client.user}')
+    
 @client.event
 async def on_message(message):
     global conversation_mode
     if message.author == client.user:
-        print(f"Sent Message: {message.content}")
         return
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
-        print(f"Hello, {message.author.name}!")
+        log_text(f"Hello, {message.author.name}!")
 
     elif message.content.startswith("$convo"):
         conversation_mode = not conversation_mode
         await message.channel.send(f"Conversation mode is now set to: {conversation_mode}")
-        print(f"Conversation mode is now set to: {conversation_mode}")
+        log_text(f"Conversation mode is now set to: {conversation_mode}")
  
     elif conversation_mode:
         response = ai.generate(message.content)
         await message.channel.send(response)
-        print(f"{message.author.name}: {message.content}")
-        print(f"{client.user.name}: {response}")
+        log_text(f"{message.author.name}: {message.content}")
+        log_text(f"{client.user.name}: {response}")
     
     elif message.content.startswith("$"):
-        await message.channel.send(ai.generate(message.content[1:]))
-        print(f"Ai response to: {message.content[1:]}")
+        response = ai.generate(message.content[1:])
+        await message.channel.send(response)
+        log_text(f"$ {message.author.name}: {message.content[1:]}")
+        log_text(f"$ {client.user.name}: {response}")
 
 client.run('MTI3MTMwMzQyMjA0OTY1Mjc3Ng.GlYDdI.7FrHPFaveCUWW01UaABK8Jx1pJrXu91C73JkBQ')
 
