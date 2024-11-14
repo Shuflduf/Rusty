2db7903291d64bfaf12bfb4ed3284d62f494dbc2
Shuflduf <shuflduf@gmail.com>
2024-11-14 13:55:49 -0700

    reset everything to rust

--- constants.py
+++
@@ -1,4 +0,0 @@
-shufl_id = "442706613107687434"
-gemini_api_key = "AIzaSyCW8kjw3V-YdNu1AX_qewTgFOHiOoQOywk"
-discord_token = "MTI3MTMwMzQyMjA0OTY1Mjc3Ng.GlYDdI.7FrHPFaveCUWW01UaABK8Jx1pJrXu91C73JkBQ"
-
